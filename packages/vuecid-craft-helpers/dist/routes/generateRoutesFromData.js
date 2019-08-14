feb7bac20497fc446d990a98dc539b777ef411e1
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-08-14 17:21:11 +0200

    [Update] vuecid-craft-helpers: Build helper

--- packages/vuecid-craft-helpers/dist/routes/generateRoutesFromData.js
+++ packages/vuecid-craft-helpers/dist/routes/generateRoutesFromData.js
@@ -1,82 +1,39 @@
-import fetch from 'node-fetch';
-import ApolloClient, { gql } from 'apollo-boost';
-import { setContext } from 'apollo-link-context';
-import { createHttpLink } from 'apollo-link-http';
-export default function generateRoutesFromData(options = {
-  api: [],
-  query: '',
+import axios from 'axios';
+import { removeTrailingSlash, verifyLeadingSlash } from '@wearelucid/vuecid-helpers';
+
+async function generateRoutesFromData(options = {
+  endpoint: '',
+  section: 'pages',
   token: '',
-  bundle: '',
-  homeSlug: 'home',
-  errorPrefix: 'error-'
+  homeSlug: 'home'
 }) {
-  const uri = 'https://cms.lucid.build';
-  const token = 'Uzwv98bNdEu0JNyOUtxxhD5d8ub6-DPXZfPsvYQXXmlWgb3Xyc6txbX92PaslqVj';
-  const httpLink = createHttpLink({
-    uri,
-    fetch
-  });
-  const authLink = setContext((_, {
-    headers
-  }) => {
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: { ...headers,
-        authorization: `Bearer ${token}`
-      }
-    };
-  });
-  const GET_PAGES = gql`
-    {
-      entries(section: [pages]) {
-        slug
-        uri
-      }
+  const {
+    token,
+    endpoint,
+    section
+  } = options; // Fetch all sections with the specified section name from your GraphQL API
+
+  let routes = await axios.post(endpoint, {
+    query: `{ entries (section: [${section}]) { uri } }`
+  }, {
+    headers: {
+      Authorization: `Bearer ${token}`
     }
-  `;
-  const client = new ApolloClient({
-    uri,
-    link: authLink.concat(httpLink)
-  });
-  client.query({
-    query: GET_PAGES
-  }).then(console.log); // // Get an array of post types, something like [ 'pages', 'posts' ]
-  // const _postTypes = options.postTypes.map(pt => pt.type)
-  //
-  // // Load and save localized option JSON's, includes all pages and posts of our languages, something we can work with :)
-  // const localizedJson = options.langs.map(l => loadLocalizedJson(`${options.dataPath}/${options.bundle}`, l.slug))
-  //
-  // // Get path from url for each post type from locaized JSON and make an array :-)
-  // const langRoutes = _postTypes.reduce((acc, type) => {
-  //   return [
-  //     // All the pages
-  //     ...acc.map(l => {
-  //       const f = l
-  //         // Kick out all the pages containing the home slug
-  //         // This could also delete a page that contains a string linke '…/home…'
-  //         // maybe a page with the permalink /pages/something/home-sweet-home
-  //         // Sadly this step is necessary since we can not redirect() with our middleware during generate
-  //         .filter(s => !s.includes(`/${options.homeSlug}`))
-  //
-  //         // Kick out error pages
-  //         .filter(s => !s.includes(`/${options.errorPrefix}`))
-  //
-  //       // Verify trailing slash so we don't get duplicate route generation
-  //       return f.map(p => verifyTrailingSlash(p))
-  //     }),
-  //     // All the posts
-  //     ...localizedJson.map(l => {
-  //       // Verify trailing slash so we don't get duplicate route generation
-  //       return l[type].map(p => verifyTrailingSlash(getPathFromUrl(p.link)))
-  //     })
-  //   ]
-  // }, []) // acc
-  //
-  // // Use root '/' for default lang
-  // const langRoutesRoot = options.langs.map(l => (l.default ? '/' : `/${l.slug}/`))
-  //
-  // return [
-  //   ...langRoutesRoot,
-  //   ...langRoutes.reduce((acc, cur) => [...acc, ...cur], []) // flatten array
-  // ]
+  }).then(result => {
+    if (result.data.data.entries) {
+      return result.data.data.entries.map(r => r.uri);
+    } else {
+      throw new Error('No sections found with this query. -> check return value from axios request: ', result);
+    }
+  }).catch(error => {
+    console.log('error: ', error); // eslint-disable-line no-console
+  }); // Kick out all the pages containing the home slug
+  // This could also delete a page that contains a string like '…/home…'
+  // maybe a page with the permalink /pages/something/home-sweet-home
+  // Sadly this step is necessary since we can not redirect() with our middleware during generate
+
+  routes = routes.filter(r => !r.includes(`/${options.homeSlug}`)).map(r => verifyLeadingSlash(removeTrailingSlash(r)));
+  return routes;
 }
+
+export default generateRoutesFromData;
