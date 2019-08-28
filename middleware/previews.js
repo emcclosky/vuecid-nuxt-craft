53d44ef0eb8cb9e3d1ddb1a76bfeb56de4fbac65
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-08-28 13:40:35 +0200

    [Update] Get newest draft by passing craft token in axios call

--- middleware/previews.js
+++ middleware/previews.js
@@ -17,11 +17,22 @@ export default async function ({ isServer, req, route, redirect, env, $axios }) 
     const routes = await $axios
       .post(
         endpoint,
-        { query: `{ entries (section: [pages]) { uri } }` },
+        { query: `{ entries(section: [pages], slug: "bart-simpson-loves-skateboarding") {
+            slug
+            title
+            ...on Pages {
+              richtext {
+                totalPages
+                content
+              }
+            }
+            }
+          }`
+        },
         { headers: { Authorization: `Bearer ${env.GRAPHQL_TOKEN}` } }
       )
       .then(result => {
-        console.log('result: ', result)
+        console.log('result: ', result.data.data.entries)
       })
       .catch(error => {
         console.error('error: ', error)
