5b0b8198b9315654072d17247e65ac74c3685789
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-10-11 17:58:57 +0200

    [Update] Header: Simplify styling

--- components/_SingleInstance/TheNavigation/TheNavigation.vue
+++ components/_SingleInstance/TheNavigation/TheNavigation.vue
@@ -14,18 +14,19 @@ export default {
   },
   mounted() {
     if (process.browser) {
-      window.addEventListener('keyup', e => {
-        this.handleKeyboardEvent(e)
-      })
+      window.addEventListener('keyup', this.listenerFunction)
     }
   },
   destroyed() {
     if (process.browser) {
-      window.removeEventListener('keyup')
+      window.removeEventListener('keyup', this.listenerFunction)
     }
   },
   methods: {
     ...mapActions('ui', ['toggleMenu', 'closeMenu']),
+    listenerFunction(e) {
+      this.handleKeyboardEvent(e)
+    },
     isHomeSlug(slug) {
       return isHomeSlug(slug)
     },
@@ -33,6 +34,7 @@ export default {
       return this.isHomeSlug(slug) ? '' : `${slug}`
     },
     handleKeyboardEvent(e) {
+      console.log('e: ', e)
       const key = e.which || e.keyCode
       if (key === 27) {
         // Esc key
@@ -52,49 +54,49 @@ export default {
     <client-only>
       <BBurger :is-active="navMenuOpen" @click.native="toggleMenu" />
     </client-only>
+
     <div :class="['TheNavigation__wrap', navMenuOpen ? 'is-open' : '']">
+      <!-- scroll-wrapper is needed to guarantee correct scrolling if list is longer than window -->
       <div v-if="menu" class="TheNavigation__scroll-wrapper">
-        <div class="TheNavigation__main-nav-wrap">
-          <ul :class="['TheNavigation__list']">
-            <li
-              v-for="(item, key) in menu"
-              :key="`nav-item-${key}`"
-              :class="[
-                'TheNavigation__item',
-                item.children && item.children.length
-                  ? 'TheNavigation__item--has-children'
-                  : ''
-              ]"
-              @click="closeMenu"
+        <ul :class="['TheNavigation__list']">
+          <li
+            v-for="(item, key) in menu"
+            :key="`nav-item-${key}`"
+            :class="[
+              'TheNavigation__item',
+              item.children && item.children.length
+                ? 'TheNavigation__item--has-children'
+                : ''
+            ]"
+            @click="closeMenu"
+          >
+            <nuxt-link
+              class="TheNavigation__link"
+              :to="$i18n.path(removeHomeSlug(item.slug))"
+              :exact="isHomeSlug(item.slug)"
             >
-              <nuxt-link
-                class="TheNavigation__link"
-                :to="$i18n.path(removeHomeSlug(item.slug))"
-                :exact="isHomeSlug(item.slug)"
+              <!-- eslint-disable-next-line vue/no-v-html -->
+              <span v-html="item.title" />
+            </nuxt-link>
+            <!-- TODO: We should be able to access the subList by tab (A11Y) -->
+            <ul
+              v-if="item.children && item.children.length"
+              class="TheNavigation__sub-list"
+            >
+              <li
+                v-for="(child, childkey) in item.children"
+                :key="`nav-item-child-${childkey}`"
+                class="TheNavigation__item TheNavigation__item--child"
+                @click="closeMenu"
               >
-                <!-- eslint-disable-next-line vue/no-v-html -->
-                <span v-html="item.title" />
-              </nuxt-link>
-              <!-- TODO: We should be able to access the subList by tab (A11Y) -->
-              <ul
-                v-if="item.children && item.children.length"
-                class="TheNavigation__sub-list"
-              >
-                <li
-                  v-for="(child, childkey) in item.children"
-                  :key="`nav-item-child-${childkey}`"
-                  class="TheNavigation__item TheNavigation__item--child"
-                  @click="closeMenu"
-                >
-                  <nuxt-link class="TheNavigation__link" :to="generateUri(item.slug, child.slug)">
-                    <!-- eslint-disable-next-line vue/no-v-html -->
-                    <span v-html="child.title" />
-                  </nuxt-link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+                <nuxt-link class="TheNavigation__link" :to="generateUri(item.slug, child.slug)">
+                  <!-- eslint-disable-next-line vue/no-v-html -->
+                  <span v-html="child.title" />
+                </nuxt-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
       <BLoader v-else />
     </div>
