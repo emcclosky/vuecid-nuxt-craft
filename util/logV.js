b2b7124563534439eae117df8f4060c57cd1d6a6
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2020-02-25 19:50:40 +0100

    [Fix] Temporary fix of non working log Verbose with bows

--- util/logV.js
+++ util/logV.js
@@ -1,16 +1,6 @@
-import bows from 'bows'
-bows.config({
-  padding: false,
-  separator: '//'
-})
-
 const logVerbose = (module, msg, val) => {
   if (process.env.LOG_VERBOSE) {
-    if (val) {
-      bows(module)(msg, val)
-    } else {
-      bows(module)(msg)
-    }
+    console.log(`logV -- ${module}: `, msg, val || '') // eslint-disable-line
   }
 }
 
